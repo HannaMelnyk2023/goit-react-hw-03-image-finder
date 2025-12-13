939bd88c32be08024e032d3932b913f08ce8c4d8
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-12-13 18:58:15 +0200

    modal: onClose Esc

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import css from './Modal.module.css';
 
 export class Modal extends Component {
+    // слухач подій на keydown, для закриття модалки по Esc
+    componentDidMount() {
+        window.addEventListener("keydown", this.handleKeyDown);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("keydown", this.handleKeyDown);
+    }
+    handleKeyDown = e => {
+        if (e.code === "Escape") {
+            this.props.onClose();
+        }
+    };
     render() {
         const { onClose, largeImageURL } = this.props;
     
